d0fa81f8010267c61b09a2fa79f0c6227e812862
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2021-11-18 18:55:25 +0300

    Use Routes and Route tag.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, useNavigate, useRoutes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import App from './App'
 
 
@@ -10,19 +10,18 @@ function Routed() {
   React.useEffect(() => {
     const referrer = document.referrer;
     if (referrer) {
-      console.log('Referrer: ', document.referrer);
       const path = new URL(document.referrer).pathname;
-      console.log('Referrer: path: ', path);
       if (path.length > 1) {
         nav(path);
       }
     }
   }, []);
 
-  let element = useRoutes([
-    { path: "/*", element: <App/> },
-  ]);
-  return element;
+  return (
+    <Routes>
+      <Route path="/*" element={<App/>}/>
+    </Routes>
+  )
 }
 
 ReactDOM.render(
